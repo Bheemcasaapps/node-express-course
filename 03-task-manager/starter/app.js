09472fa3b47e2538256a3749953b5e147e93db37
arshbhushan <arshbhushan@gmail.com>
2023-04-06 15:29:48 +0530

    new_changes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,35 +1,32 @@
-require('./db/connect' )
-
-const express =require('express')
-const app=express();
-const tasks=require('./routes/tasks')
+const express = require('express');
+const app = express()
+const tasks = require('./routes/tasks')
 const connectDB=require('./db/connect')
 require('dotenv').config()
-//middleware
 
+//middleware
 app.use(express.json())
 
+
 //routes
-app.get('/hello',(req,res)=>{
+app.get('/hello', (req, res) => {
     res.send('Task Manager App')
+})
+app.use('/api/v1/tasks', tasks)
 
-}) 
 
-app.use('/api/v1/tasks',tasks)
-  
 
-const port=3000
 
-const start=async()=>{
+
+
+const port = 3000
+
+const start=async ()=>{
     try {
-        await connectDB(process.env.MONGO_URL)
-        app.listen(port, console.log(`Server is listening on port ${port}...`))
-
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listing on port ${port}...`))
     } catch (error) {
         console.log(error);
     }
 }
 start()
-
-
-
